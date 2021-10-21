50130531570ed16df408b9d2f16cd6ec20f084c9
Giulio Scattolin <giulio.scattolin@gmail.com>
2021-10-21 20:16:34 +0200

    revert: remove TemporalOffset#isEqualTo()
    BREAKING CHANGE: same semantics as equals()

--- src/main/java/com/github/giulioscattolin/temporal/offset/TemporalOffset.java
+++ src/main/java/com/github/giulioscattolin/temporal/offset/TemporalOffset.java
@@ -35,11 +35,6 @@ public interface TemporalOffset {
     boolean isGreaterThan(TemporalOffset other);
 
     /**
-     * Returns <code>true</code> if this offset is equal to the <code>other</code> one.
-     */
-    boolean isEqualTo(TemporalOffset other);
-
-    /**
      * Returns an offset that is in the future by the given number of weeks.
      */
     TemporalOffset plusWeeks(long weeks);
